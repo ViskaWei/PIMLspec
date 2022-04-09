61a69713501ae821de049a52568810db855c5495
ViskaWei <swei20@jhu.edu>
2022-04-09 16:37:03 -0400

    add specparamIF

---
+++ spec/interface/gateway/specparamIF.py
@@ -0,0 +1,31 @@
+
+
+from .specloaderIF import WaveSkyLoaderIF
+from base.interface.gateway.baseparamIF import BaseParamIF
+
+
+class SpecParamIF(BaseParamIF):
+
+    def set_param(self, step=10, bosz_res=5000, arm="RedM", ResTune="Alex", WSKY_PATH=None, wavesky=None):
+        self.step = step
+        self.bosz_res = bosz_res
+        self.arm = arm
+        self.ResTune = ResTune
+        self.pfs_res = bosz_res * 2
+        self.load_wavesky(wavesky, WSKY_PATH)
+
+    def load_wavesky(self, wavesky, WSKY_PATH):
+        if wavesky is None:
+            self.wavesky  = WaveSkyLoaderIF(WSKY_PATH).load()
+        else:
+            self.wavesky  = wavesky
+
+    def get(self):
+        OP_MODEL = {'ResTune': {'type': self.ResTune, 'param': {'step': self.step, 'res': self.pfs_res}}}
+
+        OP_PARAM = {'arm': self.arm}
+
+        OP_DATA  =  {'WSKYPATH': self.WSKY_PATH}       
+
+        OP_OUT   =  {}
+        return OP_MODEL, OP_PARAM, OP_DATA, OP_OUT
--- spec/interface/gateway/specprocessIF.py
+++ spec/interface/gateway/specprocessIF.py
@@ -1,32 +1,19 @@
-from abc import abstractmethod
+from base.interface.gateway.baseprocessIF import ProcessIF
+
 from spec.center.crust.data.basespec import StellarSpec
 from spec.center.crust.process.specprocess import StellarSpecProcess
-from base.interface.gateway.baseprocessIF import ProcessIF
-from .specloaderIF import BoszLoaderIF, WaveSkyLoaderIF
+
+from .specloaderIF import BoszLoaderIF
+from .specparamIF import SpecParamIF
 
 class StellarSpecProcessIF(ProcessIF):
     def __init__(self) -> None:
         super().__init__()
         self.Loader   = BoszLoaderIF()
+        self.paramIF  = SpecParamIF()
+
         self.Process  = StellarSpecProcess()
         self.Storer   = None
-
-    def set_data(self, DATA_PARAM):
-        if "wavesky" in DATA_PARAM:
-            self.wavesky = DATA_PARAM["wavesky"]
-        else:
-            self.wavesky  = WaveSkyLoaderIF(DATA_PARAM["WSKY_PATH"]).load()
-        self.OP_DATA  = {"wavesky": self.wavesky}
-
-    def set_param(self, OP_PARAM):
-        self.OP_PARAM = self.paramIF(OP_PARAM)
-    
-    def set_model(self, MODEL_PARAM):
-        self.OP_MODEL = MODEL_PARAM
-
-    def paramIF(self, PARAMS):
-        #TODO create class later
-        return PARAMS
 
     def interact_on_Object(self, Spec: StellarSpec):
         super().interact_on_Object(Spec)
