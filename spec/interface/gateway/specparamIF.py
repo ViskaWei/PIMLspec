bda5dac3326e67f3f9ad0a60b745e065b6c9dc3d
ViskaWei <swei20@jhu.edu>
2022-04-10 13:58:17 -0400

    fix param

--- spec/interface/gateway/specparamIF.py
+++ spec/interface/gateway/specparamIF.py
@@ -12,8 +12,8 @@ class SpecParamIF(ParamIF):
         self.bosz_res  = self.get_arg('bosz_res', PARAM, 5000)
         self.ResTune   = self.get_arg('ResTune', PARAM, "Alex")
         self.wavesky   = self.get_arg('wavesky', PARAM, None)
-        self.WSKY_PATH = self.get_arg('WSKYPATH', PARAM, Constants.WSKY_PATH)
-        self.OBJECT_PATH = self.get_arg('OBJECTPATH', PARAM, Constants.BOSZ_PATH)
+        self.WSKY_PATH = self.get_arg('WSKY_PATH', PARAM, Constants.WSKY_PATH)
+        self.BOSZ_PATH = self.get_arg('BOSZ_PATH', PARAM, Constants.BOSZ_PATH)
 
         self.pfs_res = self.bosz_res * 2
         self.load_wavesky()
@@ -26,7 +26,7 @@ class SpecParamIF(ParamIF):
             self.wavesky  = WaveSkyLoaderIF(self.WSKY_PATH).load()
 
     def set_param_dict(self):
-        self.OBJECT = {"OBJECTPATH": self.OBJECT_PATH}
+        self.OBJECT = {"BOSZ_PATH": self.BOSZ_PATH}
 
         self.MODEL = {'ResTune': {'type': self.ResTune, 'param': {'step': self.step, 'res': self.pfs_res}}}
 
