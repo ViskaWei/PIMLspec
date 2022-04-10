8ce2cf670db97746b753fb4bce197c8fc4248012
ViskaWei <swei20@jhu.edu>
2022-04-10 13:31:50 -0400

    add multiparam

--- spec/interface/gateway/specparamIF.py
+++ spec/interface/gateway/specparamIF.py
@@ -3,20 +3,23 @@ from base.interface.gateway.baseparamIF import ParamIF
 from spec.center.crust.data.constants import Constants
 
 class SpecParamIF(ParamIF):
+    required_attribes = ["arm", "step"]
 
     def set_param(self, PARAM):
         self.arm       = self.get_arg('arm', PARAM)
         self.step      = self.get_arg('step', PARAM)
+
         self.bosz_res  = self.get_arg('bosz_res', PARAM, 5000)
         self.ResTune   = self.get_arg('ResTune', PARAM, "Alex")
-        self.wavesky   = self.get_arg('wavesky', PARAM)
+        self.wavesky   = self.get_arg('wavesky', PARAM, None)
         self.WSKY_PATH = self.get_arg('WSKYPATH', PARAM, Constants.WSKY_PATH)
         self.OBJECT_PATH = self.get_arg('OBJECTPATH', PARAM, Constants.BOSZ_PATH)
 
         self.pfs_res = self.bosz_res * 2
         self.load_wavesky()
-        
+
         self.set_param_dict()
+        
 
     def load_wavesky(self):
         if self.wavesky is None:
