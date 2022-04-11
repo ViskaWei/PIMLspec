47b62f594fdb4d3e91cef91725e7cafd588c9623
ViskaWei <swei20@jhu.edu>
2022-04-11 13:39:48 -0400

    fix param

--- spec/interface/gateway/specprocessIF.py
+++ spec/interface/gateway/specprocessIF.py
@@ -9,11 +9,11 @@ from .specparamIF import StellarSpecParamIF
 class StellarSpecProcessIF(ProcessIF):
     def __init__(self) -> None:
         super().__init__()
-        self.Loader   = BoszLoaderIF()
-        self.ParamIF  = StellarSpecParamIF()
+        self.Loader  = BoszLoaderIF()
+        self.Param   = StellarSpecParamIF()
 
-        self.Process  = StellarSpecProcess()
-        self.Storer   = None
+        self.Process = StellarSpecProcess()
+        self.Storer  = None
 
     def interact_on_Object(self, Spec: StellarSpec):
         super().interact_on_Object(Spec)
